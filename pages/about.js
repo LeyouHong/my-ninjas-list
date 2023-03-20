d88dbc70a65b21d39f61b2a8ca2230dfe8933800
Leyou Hong <lawrencehongengineer@gmail.com>
2023-03-19 21:42:05 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <p>lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...</p>
+        <p>lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...</p>
+      </div>
+    </>
+  )
+}
+
+export default About
---
+++ pages/ninjas/test.js
@@ -0,0 +1,11 @@
+const Test = () => {
+  return (
+    <div>
+      <h1>this is a test</h1>
+      <p>lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...</p>
+      <p>lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...lalala...</p>
+    </div>
+  )
+}
+
+export default Test
